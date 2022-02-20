0712ddafb94645ee16bde7590286d641a274b3e8
MinTins <enderator15@gmail.com>
2022-02-20 18:30:07 +0200

    Create donut.py

---
+++ donut.py
@@ -0,0 +1,28 @@
+# Done by MinTins and roma160
+# The code for rendering is taken from 
+# https://github.com/reeesga/donut/blob/master/donut.py
+
+{
+                         1 for b,c,d,e,f,
+                    g,q,r,F in((100,100,250,1
+                  ,2,5,45,35,__import__("math")
+                ),)for s,E,G,Q,R,S in((q*g*3/(8*(
+              e+f)),F.sin,F.cos,range,int,print),)
+             for t in Q(d)for u,v in((1+t*.08,1+t*.03
+            ),)for w,o,p,m,n,k    in((G(u),E( u),G(v),
+           E(v),[[' ']*q for _       in Q( r + 1) ], [
+           [0] * q for _ in            Q(r+1)]),)if {1
+           for i in(2*F.pi             *a/b for a in Q
+          (b))for h,y in((             G(i),E(i)),)for
+          j in(2*F.pi*a/ c            for a in Q(c))for
+           x,z,l,A in((G(j),         E(j),f+e*h,e*y),)
+           for B in(1/(g+w*l*z     +A*o),)for C,D,L in
+           ((R(q/3+s*B*(l*(p*x+o*m*z)-A*w*m)*.6), R(r/
+            1.6-s*B*(l*(m*x-o*p*z)+A*w*p)),x*h*m-w*h*z
+             -o*y+p*(w*y-h*o*z)),)if L>0 and B>k[C][
+              D]and{k[C].__setitem__( D, B), n[ C].
+                __setitem__(D,'.,-~:;=!*#$@' [ R (
+                  L*8)])}or 1}or 1 if[S('\x1b[H'
+                      ),{S(''.join(E))for E in
+                             n}] or 1
+}
